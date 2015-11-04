08a2cb49e729f28648dd70a8cad89c5e10bde977
Simone Basso <bassosimone@gmail.com>
2015-11-04 17:03:59 +0100

    random_body: only keep global-vars independent RandomBody

--- neubot_runtime/random_body.py
+++ neubot_runtime/random_body.py
@@ -19,111 +19,7 @@
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-''' Generate random data blocks for the tests '''
-
-#
-# This code is inspired by code published on the blog
-# of Jesse Noller <http://bit.ly/irj8je>.
-#
-
-import collections
-import os.path
-import random
-
-#
-# Must use WWWDIR because Python modules are not
-# reachable with Windows.  They are stored into
-# library.zip.
-#
-from . import utils_hier
-
-# Maximum depth
-MAXDEPTH = 16
-
-# Size of a block
-BLOCKSIZE = 262144
-
-def listdir(curdir, vector, depth):
-
-    ''' Make a list of all the files in a given directory
-        up to a certain depth '''
-
-    if depth > MAXDEPTH:
-        return
-
-    for entry in os.listdir(curdir):
-        entry = os.path.join(curdir, entry)
-        if os.path.isdir(entry):
-            listdir(entry, vector, depth + 1)
-        elif os.path.isfile(entry):
-            vector.append(entry)
-
-def create_base_block(length):
-
-    ''' Create a base block of length @length '''
-
-    base_block = collections.deque()
-
-    files = []
-    listdir(utils_hier.WWWDIR, files, 0)
-    random.shuffle(files)
-
-    for fpath in files:
-        fileptr = open(fpath, 'rb')
-        content = fileptr.read()
-        words = content.split()
-
-        for word in words:
-            amount = min(len(word), length)
-            word = word[:amount]
-            wordlist = list(word)
-            random.shuffle(wordlist)
-
-            base_block.append(''.join(wordlist))
-            length -= amount
-            if length <= 0:
-                break
-
-        if length <= 0:
-            break
-
-    return base_block
-
-def block_generator(size):
-
-    ''' Generator that returns blocks '''
-
-    block = create_base_block(size)
-    while True:
-        block.rotate(random.randrange(4, 16))
-        yield ''.join(block)
-
-class RandomBlocks(object):
-
-    ''' Generate blocks randomly shuffling a base block '''
-
-    def __init__(self, size=BLOCKSIZE):
-        ''' Initialize random blocks generator '''
-        self._generator = block_generator(size)
-        self.blocksiz = size
-
-    def reinit(self):
-        ''' Reinitialize the generator '''
-        self._generator = block_generator(self.blocksiz)
-
-    def get_block(self):
-        ''' Return a block of data '''
-        return self._generator.next()
-
-RANDOMBLOCKS = RandomBlocks()
-
-#
-# XXX Create and discard the first block at the very
-# beginning, so we are sure that we can fetch all the
-# needed files in the common case, i.e. when we
-# startup as root.
-#
-RANDOMBLOCKS.get_block()
+''' Random body for HTTP code '''
 
 class RandomBody(object):
 
@@ -133,24 +29,25 @@ class RandomBody(object):
      returned by its read() method.
     '''
 
-    def __init__(self, total):
+    def __init__(self, random_blocks, total):
         ''' Initialize random body object '''
-        self.total = int(total)
+        self._random_blocks = random_blocks
+        self._total = int(total)
 
     def read(self, want=None):
         ''' Read up to @want bytes '''
         if not want:
-            want = self.total
-        amt = min(self.total, min(want, RANDOMBLOCKS.blocksiz))
+            want = self._total
+        amt = min(self._total, min(want, self._random_blocks.blocksiz))
         if amt:
-            self.total -= amt
-            return RANDOMBLOCKS.get_block()[:amt]
+            self._total -= amt
+            return self._random_blocks.get_block()[:amt]
         else:
-            return ''
+            return b''
 
     def seek(self, offset=0, whence=0):
         ''' Seek stub '''
 
     def tell(self):
         ''' Tell the amounts of bytes left '''
-        return self.total
+        return self._total
