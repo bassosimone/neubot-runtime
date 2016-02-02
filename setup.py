912ac3e971b05de878af3bad8089ac4a65463176
Simone Basso <bassosimone@gmail.com>
2016-02-02 12:16:04 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Part of Neubot <https://neubot.nexacenter.org/>.
+# Neubot is free software. See AUTHORS and LICENSE for more
+# information on the copying conditions.
+
+from distutils.core import setup
+
+setup(name='neubot-runtime',
+      version='0.4.17.0',
+      description='Neubot Runtime Library',
+      author='Simone Basso, et al.',
+      author_email='bassosimone@gmail.com',
+      url='https://neubot.nexacenter.org/',
+      packages=['neubot_runtime'],
+     )
